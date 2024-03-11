b6367cfb35c9f95e5c9f58470ac925760352e17f
viethungle1 <hunglv0801@gmail.com>
2024-03-11 09:17:45 +0700

    count access feature

---
+++ src/main/java/com/example/springcountview/model/Counter.java
@@ -0,0 +1,24 @@
+package com.example.springcountview.model;
+
+public class Counter {
+    private int count;
+
+    public Counter() {
+    }
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public void increment() {
+        count++;
+    }
+}
